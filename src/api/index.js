a048fa2607e467a923b452719b50a053cb2df02e
Mr.Yogesh <yogeshvanzara98@gmail.com>
2023-01-14 21:59:00 +0530

    bug

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/" });
-// const API = axios.create({ baseURL: "https://memories-evff.onrender.com" });
+// const API = axios.create({ baseURL: "http://localhost:5000/" });
+const API = axios.create({ baseURL: "https://memories-evff.onrender.com" });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
     req.headers.Authorization = `Bearer ${
